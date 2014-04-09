67e6c598f088eacc09be46d9cb70b9aac93d657f
Synature-Jitthapong <jitthapong@synaturegroup.com>
2014-04-09 20:23:44 +0700

    edit logger class

--- src/com/j1tth4/mobile/util/Logger.java
+++ src/com/j1tth4/mobile/util/Logger.java
@@ -11,38 +11,25 @@ import android.content.Context;
 import android.util.Log;
 
 public abstract class Logger {
-	public static final String FILE_EXTENSION = ".txt";
-	private Context mContext;
-	private String mLogDir;
-	private String mFileName;
-	private SimpleDateFormat mTimeFormat;
-	private SimpleDateFormat mDateFormat;
-	private Date date;
-	
-	public Logger(Context c, String logDir, String fileName){
-		mContext = c;
-		mLogDir = logDir;
-		mFileName = fileName;
 
-		date = new Date();
-		mTimeFormat = 
-				new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
-		mDateFormat = 
-				new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-		mFileName += "_" + mDateFormat.format(date) + FILE_EXTENSION;
-	}
+	public static final String FILE_EXTENSION = ".txt";
 	
-	public void appendLog(String mesg) {
-		
-		FileManager fileManager = new FileManager(mContext, mLogDir);
-		String logFile = fileManager.getFile(mFileName).getPath();
+	public static void appendLog(Context c, String logDir, String fileName, String mesg) {
+		Date date = new Date();
+		SimpleDateFormat timeFormat = 
+				new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+		SimpleDateFormat dateFormat = 
+				new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+		fileName += dateFormat.format(date) + FILE_EXTENSION;
+		FileManager fileManager = new FileManager(c, logDir);
+		String logFile = fileManager.getFile(fileName).getPath();
 		
 		try {
 			// BufferedWriter for performance, true to set append to file flag
 			BufferedWriter buf = new BufferedWriter(new FileWriter(logFile,
 					true));
 			buf.newLine();
-			buf.append(mTimeFormat.format(date) + " " + mesg);
+			buf.append(timeFormat.format(date) + " " + mesg);
 			buf.close();
 		} catch (IOException e) {
 			Log.d("LOGGER", e.getMessage());
